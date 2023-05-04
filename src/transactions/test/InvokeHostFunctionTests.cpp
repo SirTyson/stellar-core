89ed819f93af291b6f9e6f21e16ccb5c831bcfa9
Garand Tyson <garand@stellar.org>
2023-05-04 16:05:43 -0700

    Compiles with XDR changes

--- src/transactions/test/InvokeHostFunctionTests.cpp
+++ src/transactions/test/InvokeHostFunctionTests.cpp
@@ -110,15 +110,20 @@ submitTxToDeployContract(Application& app, Operation const& deployOp,
         LedgerTxn ltx2(app.getLedgerTxnRoot());
         auto ltxe2 = loadContractCode(ltx2, expectedWasmHash);
         REQUIRE(ltxe2);
-        REQUIRE(ltxe2.current().data.contractCode().code == expectedWasm);
+
+        auto const& body = ltxe2.current().data.contractCode().body;
+        REQUIRE(body.t() == DATA_ENTRY);
+        REQUIRE(body.code() == expectedWasm);
     }
     // verify contract code reference is correct
     {
         LedgerTxn ltx2(app.getLedgerTxnRoot());
         auto ltxe2 = loadContractData(ltx2, contractID, sourceRefKey);
         REQUIRE(ltxe2);
-        REQUIRE(ltxe2.current().data.contractData().val ==
-                makeWasmRefScContractCode(expectedWasmHash));
+
+        auto const& body = ltxe2.current().data.contractData().body;
+        REQUIRE(body.t() == DATA_ENTRY);
+        REQUIRE(body.val() == makeWasmRefScContractCode(expectedWasmHash));
     }
 }
 
@@ -295,7 +300,10 @@ TEST_CASE("contract storage", "[tx][soroban]")
         if (val)
         {
             REQUIRE(ltxe);
-            REQUIRE(ltxe.current().data.contractData().val == *val);
+
+            auto const& body = ltxe.current().data.contractData().body;
+            REQUIRE(body.t() == DATA_ENTRY);
+            REQUIRE(body.val() == *val);
         }
         else
         {
