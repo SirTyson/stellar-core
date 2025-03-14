53ccee5fd3727f45859bafbf60649b62f71df0bc
Garand Tyson <garand@stellar.org>
2025-03-14 14:00:07 -0700

    POC compressed tx set

--- src/overlay/Peer.h
+++ src/overlay/Peer.h
@@ -287,6 +287,7 @@ class Peer : public std::enable_shared_from_this<Peer>,
     void recvGetTxSet(StellarMessage const& msg);
     void recvTxSet(StellarMessage const& msg);
     void recvGeneralizedTxSet(StellarMessage const& msg);
+    void recvCompressedGeneralizedTxSet(StellarMessage const& msg);
     void recvTransaction(CapacityTrackedMessage const& msgTracker);
     void recvGetSCPQuorumSet(StellarMessage const& msg);
     void recvSCPQuorumSet(StellarMessage const& msg);
