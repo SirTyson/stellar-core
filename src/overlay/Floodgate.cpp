1445bc27e3e112f1c44f49cb1b44e98ce604c8e3
Garand Tyson <garand@stellar.org>
2025-03-27 15:15:06 -0700

    Removed peer authenticated assert in Floodgate::broadcast

--- src/overlay/Floodgate.cpp
+++ src/overlay/Floodgate.cpp
@@ -86,6 +86,7 @@ bool
 Floodgate::broadcast(std::shared_ptr<StellarMessage const> msg,
                      std::optional<Hash> const& hash)
 {
+    releaseAssert(threadIsMain());
     ZoneScoped;
     if (mShuttingDown)
     {
@@ -122,7 +123,7 @@ Floodgate::broadcast(std::shared_ptr<StellarMessage const> msg,
     {
         // Assert must hold since only main thread is allowed to modify
         // authenticated peers and peer state during drop
-        peer.second->assertAuthenticated();
+        // peer.second->assertAuthenticated();
 
         bool pullMode = msg->type() == TRANSACTION;
 
