0cd1fb032448717e00d7fe7a14c0dcc79a1ea756
Garand Tyson <garand@stellar.org>
2025-10-03 15:57:09 -0700

    Add experimental flag for dalek library

--- src/main/ApplicationImpl.cpp
+++ src/main/ApplicationImpl.cpp
@@ -54,6 +54,7 @@
 #include "util/GlobalChecks.h"
 #include "util/LogSlowExecution.h"
 #include "util/Logging.h"
+#include "util/ProtocolVersion.h"
 #include "util/StatusManager.h"
 #include "util/Thread.h"
 #include "util/TmpDir.h"
@@ -799,6 +800,15 @@ ApplicationImpl::start()
     mStarted = true;
 
     mLedgerManager->loadLastKnownLedger(/* restoreBucketlist */ true);
+
+    // Check if we're already on protocol V_24 or later and enable Rust Dalek
+    auto const& lcl = mLedgerManager->getLastClosedLedgerHeader();
+    if (protocolVersionStartsFrom(lcl.header.ledgerVersion,
+                                  ProtocolVersion::V_24))
+    {
+        PubKeyUtils::enableRustDalekVerify();
+    }
+
     startServices();
 }
 
